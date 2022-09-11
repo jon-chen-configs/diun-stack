2177252ee4b2828de034ced51b2d482c449279c8
Jonathan Chen <jonchen228@gmail.com>
2022-09-11 14:57:07 -0400

    export ENV_DIR for stack deploy script.

--- diun.deploy.sh
+++ diun.deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 SCRIPTS_DIR=../../scripts
-ENV_DIR=../../../compose/conf/swarm-env-config
+export ENV_DIR=../../../compose/conf/swarm-env-config
 COMPOSE_FILE=diun-swarm.yml STACK_NAME=diun-swarm-test ENV_FILE=${ENV_DIR}/swarm.env ${SCRIPTS_DIR}/stack.deploy.sh
