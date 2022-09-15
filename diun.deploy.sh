1ade1f240c1ab9411baa898193052032c1ba53f5
Jonathan Chen <jonchen228@gmail.com>
2022-09-15 12:53:17 -0400

    standardize deploy/remove shell scripts

--- diun.deploy.sh
+++ diun.deploy.sh
@@ -3,8 +3,8 @@
 WD=$(dirname $0)
 SCRIPTS_DIR=${WD}/../../scripts
 export ENV_DIR=${WD}/../../../compose/conf/swarm-env-config
+export ENV_FILE=${ENV_DIR}/swarm.env
 export COMPOSE_FILE=${WD}/diun-swarm.yml 
 export STACK_NAME=diun-swarm-test 
-export ENV_FILE=${ENV_DIR}/swarm.env
 
 ${SCRIPTS_DIR}/stack.deploy.sh
--- diun.remove.sh
+++ diun.remove.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-SCRIPTS_DIR=../../scripts
-STACK_NAME=diun-swarm ${SCRIPTS_DIR}/stack.remove.sh
+WD=$(dirname $0)
+SCRIPTS_DIR=${WD}/../../scripts
+export STACK_NAME=diun-swarm 
+
+${SCRIPTS_DIR}/stack.remove.sh
